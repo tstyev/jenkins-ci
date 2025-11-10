2d3ccc98edde13c667d819a4131a4281708c6f35
tstyev <tst.yev@gmail.com>
2025-11-10 09:24:51 +0300

    Добавлен тестовый класс

---
+++ src/test/java/school/redrover/My2Test.java
@@ -0,0 +1,12 @@
+package school.redrover;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class My2Test {
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(true);
+    }
+}
