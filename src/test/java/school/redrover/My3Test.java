d39799b2d69fa3f5161c3fda5d3b4e19aa0db10e
tstyev <tst.yev@gmail.com>
2025-11-10 09:27:08 +0300

    Добавлен тестовый класс

---
+++ src/test/java/school/redrover/My3Test.java
@@ -0,0 +1,13 @@
+package school.redrover;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class My3Test extends BaseTest {
+
+    @Test
+    public void test3() {
+        Assert.assertTrue(true);
+    }
+}
