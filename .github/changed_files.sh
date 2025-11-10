3936bb92bba34fd8e4d654aa5f26c7346063a9d2
tstyev <tst.yev@gmail.com>
2025-11-10 08:59:23 +0300

    Refactor CI

--- .github/changed_files.sh
+++ .github/changed_files.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-# Находим общий предок ветки с main
-BASE=$(git merge-base origin/$GITHUB_HEAD_REF origin/main)
+# Находим ветку (pull request или push)
+BRANCH=${GITHUB_HEAD_REF:-${GITHUB_REF_NAME}}
+
+# Находим общий предок с main
+BASE=$(git merge-base origin/$BRANCH origin/main)
 
 # Берём все добавленные, изменённые, переименованные или скопированные .java файлы
-CHANGED_FILES=$(git diff --diff-filter=ACMRT --name-only $BASE origin/$GITHUB_HEAD_REF | grep '\.java$' | tr '\n' ';')
+CHANGED_FILES=$(git diff --diff-filter=ACMRT --name-only $BASE origin/$BRANCH | grep '\.java$' | tr '\n' ';')
 
-# Если среди них есть хотя бы один не тестовый файл → FULL_RUN
+# Если среди них есть хотя бы один нетестовый файл → FULL_RUN
 if echo "$CHANGED_FILES" | grep -q -v "Test\.java"; then
   echo "LIST_OF_CHANGED_FILES=FULL_RUN" >> $GITHUB_ENV
 else
