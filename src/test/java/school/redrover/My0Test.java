278af14698bff0cab5c0e41e2bb8a1b9dc5e72f9
YevgenyN <163114876+tstyev@users.noreply.github.com>
2025-11-11 23:42:13 +0300

    Один класс удалил, один изменил, один добавил (#3)
    * Один класс удалил, один изменил, один добавил
    
    * Обновил экшн

--- src/test/java/school/redrover/My0Test.java
+++ src/test/java/school/redrover/My0Test.java
@@ -4,10 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-public class My3Test extends BaseTest {
+public class My0Test extends BaseTest {
 
     @Test
-    public void test3() {
+    public void test0() {
         Assert.assertTrue(true);
     }
 
