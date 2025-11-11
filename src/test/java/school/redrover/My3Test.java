268fc5d5f71a49dab098596aa390173e9b578639
tstyev <tst.yev@gmail.com>
2025-11-12 02:38:05 +0300

    Refactoring

--- src/test/java/school/redrover/My3Test.java
+++ src/test/java/school/redrover/My3Test.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-public class My4Test extends BaseTest {
+public class My3Test extends BaseTest {
 
     @Test
     public void test2() {
