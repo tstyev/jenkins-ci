4ce7c65a7c3329f20cf2d38481802c65813f9d6e
tstyev <tst.yev@gmail.com>
2025-11-11 23:45:52 +0300

    Обновил и добавил

--- src/test/java/school/redrover/My3Test.java
+++ src/test/java/school/redrover/My3Test.java
@@ -4,11 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-public class My0Test extends BaseTest {
+public class My3Test extends BaseTest {
 
     @Test
-    public void test0() {
+    public void test2() {
         Assert.assertTrue(true);
     }
-
 }
