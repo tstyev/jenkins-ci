b4e42d67a02cddc4d54b147482d289d6854aeaab
YevgenyN <163114876+tstyev@users.noreply.github.com>
2025-11-12 03:40:14 +0300

    Refactoring (#15)
    * Refactoring
    
    * Refactoring
    
    * Refactoring

--- src/test/java/school/redrover/My0Test.java
+++ src/test/java/school/redrover/My0Test.java
@@ -2,13 +2,18 @@ package school.redrover;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import school.redrover.common.BaseTest;
 
-public class My1Test extends BaseTest {
+public class My0Test extends BaseTest {
 
     @Test
-    public void test1() {
+    public void test2() {
         Assert.assertTrue(true);
     }
+
+    @Test
+    public void test22() {
+        Assert.assertTrue(true);
+    }
+
 }
