cc6d830c6f69e12e18f7d85a266c306342790f3d
tstyev <tst.yev@gmail.com>
2025-11-12 03:46:51 +0300

    Refactoring

--- src/test/java/school/redrover/common/Test1Test.java
+++ src/test/java/school/redrover/common/Test1Test.java
@@ -3,7 +3,7 @@ package school.redrover.common;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Test1Test {
+public class Test1Test extends BaseTest {
     @Test
     public void test1() {
         Assert.assertTrue(true);
