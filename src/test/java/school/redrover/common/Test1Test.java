736dc608073301bb269c14e31b28c387adb5d407
tstyev <tst.yev@gmail.com>
2025-11-12 03:45:41 +0300

    Refactoring

---
+++ src/test/java/school/redrover/common/Test1Test.java
@@ -0,0 +1,11 @@
+package school.redrover.common;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1Test {
+    @Test
+    public void test1() {
+        Assert.assertTrue(true);
+    }
+}
