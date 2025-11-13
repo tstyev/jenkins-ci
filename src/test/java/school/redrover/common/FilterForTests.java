75cf8ea777e79e65581e8b86dd76fb26c91fb4c2
tstyev <tst.yev@gmail.com>
2025-11-13 06:22:33 +0300

    Refactoring

--- src/test/java/school/redrover/common/FilterForTests.java
+++ src/test/java/school/redrover/common/FilterForTests.java
@@ -2,46 +2,27 @@ package school.redrover.common;
 
 import org.testng.*;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class FilterForTests implements IMethodInterceptor {
 
     @Override
     public List<IMethodInstance> intercept(List<IMethodInstance> methods, ITestContext context) {
+        String files = System.getenv("LIST_OF_CHANGED_FILES");
 
-        String changed = System.getenv("CHANGED_TEST_FILES");
-        String deleted = System.getenv("DELETED_TEST_FILES");
+        if (files != null) {
+            List<String> entryList = Arrays.stream(files.split(";"))
+                    .toList();
 
-        if (changed != null || deleted != null) {
-            Set<String> changedFiles = new HashSet<>();
-            Set<String> deletedFiles = new HashSet<>();
-
-            for (String f : changed.split(";")) {
-                String path = f.trim();
-                if (!path.isEmpty()) {
-                    changedFiles.add(path);
-                }
-            }
-
-            for (String f : deleted.split(";")) {
-                String path = f.trim();
-                if (!path.isEmpty()) {
-                    deletedFiles.add(path);
-                }
-            }
-
-            System.out.println("Deleted files: " + deletedFiles);
-            System.out.println("Other files: " + changedFiles);
-
-            boolean hasNonTest = Stream.concat(deletedFiles.stream(), changedFiles.stream())
-                    .anyMatch(f -> !f.endsWith("Test.java"));
-
-            if (hasNonTest) {
-                return methods;
-            }
+            Set<String> changedFiles = entryList.stream()
+                    .filter(e -> e.startsWith("A="))
+                    .map(e -> e.substring(2))
+                    .collect(Collectors.toSet());
 
             Map<Class<?>, String> classMap = methods.stream()
                     .map(IMethodInstance::getMethod)
@@ -53,9 +34,9 @@ public class FilterForTests implements IMethodInterceptor {
                             (pathA, pathB) -> pathA
                     ));
 
-            return methods.stream()
-                    .filter(method -> changedFiles.contains(classMap.get(method.getMethod().getTestClass().getRealClass())))
-                    .collect(Collectors.toList());
+            if (classMap.values().containsAll(changedFiles)) {
+                return methods.stream().filter(method -> changedFiles.contains(classMap.get(method.getMethod().getTestClass().getRealClass()))).collect(Collectors.toList());
+            }
         }
 
         return methods;
