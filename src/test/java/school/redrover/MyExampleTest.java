69547a1a23fac67697755e434f23764344837466
tstyev <tst.yev@gmail.com>
2025-11-13 06:43:46 +0300

    Refactoring

--- src/test/java/school/redrover/MyExampleTest.java
+++ src/test/java/school/redrover/MyExampleTest.java
@@ -4,11 +4,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-public class My3Test extends BaseTest {
-
+public class MyExampleTest extends BaseTest {
     @Test
-    public void test3() {
+    public void test1() {
         Assert.assertTrue(true);
     }
-
 }
